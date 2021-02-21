8293f47d12927da16ddb7c4bdf6c950a8257b11e
Elvina <elvinashm@gmail.com>
2021-02-21 21:37:39 +0600

    events updated

--- src/main/scala/w6/Event/CosmeticsShopEvent.scala
+++ src/main/scala/w6/Event/CosmeticsShopEvent.scala
@@ -2,13 +2,12 @@ package w6.Event
 
 trait CosmeticsShopEvent
 
-case class CreateNewCosmeticCommand(orderID: String,
-                                    itemName: String,
-                                    companyName: String) extends CosmeticsShopEvent
+case class CreateNewCosmeticEvent(orderID: String, itemName: String, companyName: String)
+          extends CosmeticsShopEvent
 
-case class FindCosmeticsItemEvent(orderID: String,
-                                  itemName: Option[String],
-                                  companyName: Option[String]) extends CosmeticsShopEvent
+case class FindCosmeticsItemEvent(orderID: String, itemName: Option[String], companyName: Option[String])
+          extends CosmeticsShopEvent
 
-case class OrderedCosmetics(orderID: String) extends CosmeticsShopEvent
-case class SellCosmeticsEvent(orderID: String) extends CosmeticsShopEvent
+case class OrderedCosmeticsEvent(orderID: String) extends CosmeticsShopEvent
+case class DeletedFromOrderCosmeticsEvent(orderID: Option[String]) extends CosmeticsShopEvent
+case class CanceledOrderEvent(orderID: String) extends CosmeticsShopEvent
